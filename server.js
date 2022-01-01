138f178841971c9c09b6586ce697abb782215404
thatojadezweni <thatojadezweni@gmail.com>
2022-01-01 21:19:13 +0200

    email stuff setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 let express = require("express");
 let helmet = require("helmet");
 let session = require("express-session");
+let nodemailer = require("nodemailer");
 
 
 // Code
@@ -15,7 +16,8 @@ let hour = 3600000;
 const app = express();
 
 app.use(helmet());
-
+app.use(express.json());
+app.use(express.static("public"));
 app.use(session({
     name: 'session',
     secret: process.env.SESSION_SECRET,
@@ -28,11 +30,21 @@ app.use(session({
     }
 }));
 
-app.use(express.static("public"));
-
 app.listen(3000, () => console.log("Server started"));
 
-app.post('/mail', (req, res) => {
-    console.log(req.body);
-    res.send("Thank you for your message");
+//TODO: finish send email functionality
+
+app.post('/mail', async (req, res) => {
+    res.status.json({
+        message: "Not supported"
+    });
 });
+
+// Helper functions
+
+let validateEmail = (email) => {
+    if (!email)
+        return false;
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
